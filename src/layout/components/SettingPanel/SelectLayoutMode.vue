5812e750a32dfd8701295b957a71b92f34ed8e20
荒 <1207588603@qq.com>
2024-04-12 11:40:14 +0800

    feat：三种布局配置小图标

--- src/layout/components/SettingPanel/SelectLayoutMode.vue
+++ src/layout/components/SettingPanel/SelectLayoutMode.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="select-layout-mode">
     <el-tooltip content="侧栏模式">
-      <el-container class="layout-mode side" :class="{ active: isSide }">
+      <el-container class="layout-mode side" :class="{ active: isSide }" @click="setLayoutMode(LayoutModeEnum.SIDE)">
         <el-aside />
         <el-container>
           <el-header />
+          <el-main />
+        </el-container>
+      </el-container>
+    </el-tooltip>
+
+    <el-tooltip content="顶部模式">
+      <el-container class="layout-mode top" :class="{ active: isTop }" @click="setLayoutMode(LayoutModeEnum.TOP)">
+        <el-header />
+        <el-main />
+      </el-container>
+    </el-tooltip>
+
+    <el-tooltip content="混合模式">
+      <el-container class="layout-mode mix" :class="{ active: isMix }" @click="setLayoutMode(LayoutModeEnum.MIX)">
+        <el-header />
+        <el-container>
+          <el-aside />
           <el-main />
         </el-container>
       </el-container>
@@ -14,14 +31,21 @@
 
 <script setup lang="ts">
 defineOptions({ name: 'SelectLayoutMode' })
+import { LayoutModeEnum } from '@/enums'
 
 const isSide = ref(true)
+const isTop = ref(false)
+const isMix = ref(false)
+
+function setLayoutMode(layoutMode: LayoutModeEnum) {
+  useModal().msgWarning(`${layoutMode} 布局正在努力开发中...`)
+}
 </script>
 
 <style lang="scss" scoped>
 .select-layout-mode {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
 }
 
 .layout-mode {
@@ -33,6 +57,9 @@ const isSide = ref(true)
   overflow: hidden;
   &:hover {
     border: 2px solid var(--el-color-primary);
+  }
+  & ~ .layout-mode {
+    margin-left: 16px;
   }
 }
 
@@ -54,7 +81,28 @@ const isSide = ref(true)
     background-color: var(--el-fill-color-darker);
   }
   .el-aside {
-    background-color: var(--app-menu-bg-color);
+    background-color: var(--el-color-primary);
+  }
+  .el-main {
+    background-color: var(--el-fill-color-lighter);
+  }
+}
+/* 顶部布局的样式 */
+.top {
+  .el-header {
+    background-color: var(--el-color-primary);
+  }
+  .el-main {
+    background-color: var(--el-fill-color-lighter);
+  }
+}
+/* 混合布局的样式 */
+.mix {
+  .el-header {
+    background-color: var(--el-fill-color-darker);
+  }
+  .el-aside {
+    background-color: var(--el-color-primary);
   }
   .el-main {
     background-color: var(--el-fill-color-lighter);
