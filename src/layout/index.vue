a60c5d15fbcf66ec8721f9a3825021ca613fd3f5
Legendary <1207588603@qq.com>
2024-04-12 21:32:42 +0800

    fix：布局模式样式冲突

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -10,6 +10,7 @@
 
 <script setup lang="ts">
 defineOptions({ name: 'Layout' })
+import { LayoutModeEnum } from '@/enums'
 import SideLayout from './SideLayout/index.vue'
 import TopLayout from './TopLayout/index.vue'
 import { SettingPanel } from '@/layout/components'
@@ -19,14 +20,18 @@ useResize()
 
 const appStore = useApp()
 const settingStore = useSetting()
-console.log('settingStore.isSide: ', settingStore.isSide)
 
 watchEffect(() => {
+  // 解决色彩样式冲突
   const classes = ['showGreyMode', 'showColorWeakness']
   const rootClassList = document.documentElement.classList
   classes.forEach((className) => rootClassList.contains(className) && rootClassList.remove(className))
   if (settingStore.showGreyMode) rootClassList.add('showGreyMode')
   if (settingStore.showColorWeakness) rootClassList.add('showColorWeakness')
+
+  // 解决布局模式样式冲突
+  const layoutClasses = [LayoutModeEnum.SIDE, LayoutModeEnum.MIX, LayoutModeEnum.TOP].map((item) => `layout-mode-${item}`)
+  layoutClasses.forEach((className) => rootClassList.contains(className) && rootClassList.remove(className))
   rootClassList.add(`layout-mode-${settingStore.layoutMode}`)
 })
 </script>
