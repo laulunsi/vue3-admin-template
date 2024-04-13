42a265981ab2019cf2c7a095edce9a597243ba5c
Legendary <1207588603@qq.com>
2024-04-13 14:47:10 +0800

    perf：富文本编辑器组件

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -6,14 +6,10 @@
 </template>
 <script lang="ts" setup>
 import '@wangeditor/editor/dist/css/style.css'
-import { onBeforeUnmount, ref, shallowRef } from 'vue'
+import { onBeforeUnmount, ref, shallowRef, CSSProperties } from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 import type { IEditorConfig, IDomEditor, IToolbarConfig } from '@wangeditor/editor'
 import toolbarKeys from './toolbarKeys'
-import { CSSProperties } from 'vue'
-
-/** 父子双向绑定数据字段 */
-const modelValue = defineModel({ default: '', type: String })
 
 const props = defineProps({
   /** 工具栏中需要隐藏的菜单的 key */
@@ -25,6 +21,9 @@ const props = defineProps({
   /** 编辑器内容区域高度 */
   height: { type: Number, default: 300 },
 })
+
+/** 父子双向绑定数据字段 */
+const modelValue = defineModel({ default: '', type: String })
 
 /** 编辑器实例，必须用 shallowRef */
 const editorRef = shallowRef<IDomEditor>()
@@ -45,17 +44,18 @@ const handleCreated = (editor: IDomEditor) => (editorRef.value = editor)
 const styles = computed<CSSProperties>(() => ({ height: `${props.height}px`, overflow: 'hidden' }))
 
 /** 自定义图片上传逻辑 */
-async function handleImageUpload(file: File, insertFn: (fileURL: string, type: string) => void) {
+async function handleImageUpload(file: File, insertFn: (fileURL: string, desc: string) => void) {
   const fileSize = file.size // 单位 bit
   const blobURL = URL.createObjectURL(file) // 此处可以替换为真正的图片上传逻辑
-  insertFn(blobURL, 'img') // 调用inserFn函数将url加载到富文本编辑器中
+  insertFn(blobURL, 'image') // 调用inserFn函数将url加载到富文本编辑器中
 }
 
 /** 自定义视频上传逻辑 */
-async function handleVideoUpload(file: File, insertFn: (fileURL: string, type: string) => void) {
+async function handleVideoUpload(file: File, insertFn: (fileURL: string, desc: string) => void) {
   console.log('上传视频', file)
+  const blobURL = URL.createObjectURL(file)
+  insertFn(blobURL, 'video')
 }
-
 /**
  * 自定义粘贴。可阻止编辑器的默认粘贴，实现自己的粘贴逻辑
  * https://www.wangeditor.com/v5/editor-config.html#custompaste
@@ -65,7 +65,7 @@ function handleCustomPaste(editor: IDomEditor, event: ClipboardEvent, callback: 
   callback(true)
 }
 
-// 组件销毁时，也及时销毁编辑器
+/** 组件销毁时，也及时销毁编辑器 */
 onBeforeUnmount(() => editorRef.value && editorRef.value.destroy())
 </script>
 
